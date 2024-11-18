628c4a272a17d797036aac4fab912ef40c8fa116
vladgibeskul <wgv96300@gmail.com>
2024-11-18 18:31:39 +0200

    2_zadanie

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ввод х=");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Ввод у=");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write($"sum={Sum(x, y)}");
+            Console.ReadKey();
+        }
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
