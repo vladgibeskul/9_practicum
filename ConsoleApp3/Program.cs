a41e6dfbb4a2adca92935d547384b0188d7875de
vladgibeskul <wgv96300@gmail.com>
2024-11-18 18:52:25 +0200

    3_zadanie

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        public static void Func()
+        {
+            Console.Write("a=");
+            int x = int.Parse(Console.ReadLine());
+            double b = 12 * Math.Pow(x, 2) + 7 * x - 6;
+            Console.WriteLine($"b={b:f2}");
+
+        }
+    }
+}
